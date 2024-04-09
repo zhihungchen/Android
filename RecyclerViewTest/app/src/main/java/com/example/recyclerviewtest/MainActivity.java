e9ca958251e9c5f7f338922bee521462076b1b4d
zhihungchen <zhihungchen@gmail.com>
2024-04-09 10:28:19 +0800

    fisrt commit

---
+++ RecyclerViewTest/app/src/main/java/com/example/recyclerviewtest/MainActivity.java
@@ -0,0 +1,102 @@
+package com.example.recyclerviewtest;
+
+
+import android.media.Image;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private List<Fruit> fruitList = new ArrayList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        initFruits();
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        FruitAdapter adapter = new FruitAdapter(fruitList);
+        recyclerView.setAdapter(adapter);
+
+
+    }
+
+    private void initFruits() {
+        for (int i = 0; i < 5; i++) {
+
+            Fruit apple = new Fruit("Apple", R.drawable.apple);
+            fruitList.add(apple);
+            Fruit banana = new Fruit("Banana", R.drawable.banana);
+            fruitList.add(banana);
+            Fruit orange = new Fruit("Orange", R.drawable.orange);
+            fruitList.add(orange);
+            Fruit watermelon = new Fruit("Watermelon", R.drawable.watermelon);
+            fruitList.add(watermelon);
+            Fruit strawberry = new Fruit("Strawberry", R.drawable.strawberry);
+            fruitList.add(strawberry);
+        }
+
+    }
+}
+
+// Adapter
+class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.ViewHolder> {
+
+    private List<Fruit> mFruitList;
+
+    public FruitAdapter(List<Fruit> mFruitList) {
+        this.mFruitList = mFruitList;
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView fruitImage;
+        TextView fruitName;
+        public ViewHolder(@NonNull View view) {
+            super(view);
+            View fruitView = view;
+            fruitImage = (ImageView) view.findViewById(R.id.fruit_image);
+            fruitName = (TextView) view.findViewById(R.id.fruit_name);
+
+        }
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fruit_item, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Fruit fruit = mFruitList.get(position);
+        holder.fruitImage.setImageResource(fruit.getImageId());
+        holder.fruitName.setText(fruit.getName());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mFruitList.size();
+    }
+
+
+}
